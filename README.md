6b3e71974c6db6273419d623505a38e5980c1a71
PereiraGus <gustavo.oito@hotmail.com>
2024-04-13 18:20:37 -0300

    Versão 0.1

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # wordcloud-filmes
 Script Python que gera uma nuvem de palavras baseada nos gostos cinematográficos do usuário.
+
+![Versão 0.1](https://img.shields.io/badge/vers%C3%A3o-0.1-yellow
+)
+
+## Como utilizar?
+
+### Requisitos
+- Python 3
+- Pip
+- Excel (desktop ou online) ou qualquer outro programa capaz de gerar arquivos no formato ".xlsx"
+- Uma conta no [The Movie Database](https://www.themoviedb.org/) (cadastro simples e gratuito)
+
+### 1. Preparar arquivo
+Crie um arquivo .xlsx (formato Excel 2007-365) com as colunas ```Filme``` e ```Propabilidade de Assistir```.
+
+Na coluna de ```Filme```, informe o nome de um filme para incluir seus temas no Wordcloud. Caso necessite de referência, utilize o [The Movie Database](https://www.themoviedb.org/search?query=).
+
+Na coluna ```Propabilidade de Assistir``` escolha um número de **0 à 5** determinando a probalidade de você assistí-lo ou, caso já o tenha feito, o quanto você gostou dele.
+
+Para referência, utilize o arquivo ```gusHorrorExample.xlsx```, incluso na pasta ```/planilhas``` desse projeto.
+
+### 2. Preparar o ambiente virtual (venv)
+Iremos instalar as bibliotecas necessárias para o script em um ambiente virtual (venv), para evitar conflitos de versões e compatibilidade.
+
+Para criá-lo, digite ```python -m venv venv``` na raíz do projeto.
+
+Para entrar nele, utilizamos ```source venv/bin/activate```
+
+A partir daqui, podemos instalar os pacotes necessários usando ```python -m pip install -r requirements.txt```
+
+Após download e instalação dos pacotes, teremos todas as bibliotecas necessárias no nosso projeto.
+
+**Quando desejar sair do programa**, use ```deactivate``` para sair do ambiente virtual. Para reentrar neste, utilize o comando "source ..." novamente.
+
+*Obs:* É possível criar e acessar ambientes virtuais de outras maneiras, inclusive através de IDEs. Consulte a documentação da sua ferramenta escolhida para saber mais.
+
+### 3 Definir chave do The Movies Database
+Logue em sua conta do The Movie Database em seu [portal oficial](https://www.themoviedb.org/login?to=read_me&redirect=%2Freference%2Fintro%2Fauthentication) e copie sua chave de autenticação.
+
+Copie o arquivo ```.env.template``` e nomeie essa cópia de ```.env```. Então, retire o comentário do parâmetro ```API_KEY``` e cole sua chave de api neste, conforme exemplo:
+```API_KEY="XPTO..."```
+
+O arquivo ```.env``` está inclúido no ```.gitignore```, ou seja, não será carregado no repositório Git, protegendo sua chave de API.
+
+### 4. Executando
+Com o terminal ou IDE dentro do ambiente virtual com os arquivos necessários, execute o arquivo ```main.py```.
+
--- main.py
+++ main.py
@@ -1,13 +1,135 @@
+import matplotlib.pyplot as plt
 import requests as req
 import pandas as pd
+from dotenv import load_dotenv
+import os
+from wordcloud import WordCloud
+import matplotlib.pyplot as pl
+from datetime import datetime as dt
+from pick import pick
+from progressbar import progressbar
+# openpyxl
 
-url = "https://api.themoviedb.org/3/authentication"
+load_dotenv()
+sess = req.Session()
+arquivo = None
 
-headers = {
-    "accept": "application/json",
-    "Authorization": "Bearer"
-}
+print("""
+==============================================================
+========= Gerador de Wordcloud a partir de filmes ============
+========================= PereiraGus +====== v0.1 ============
+==============================================================
+""")
 
-response = requests.get(url, headers=headers)
 
-print(response.text)
+def ler_arquivo():
+    print("Escolha um arquivo para extrair seus filmes e suas avaliações:")
+    arquivos = os.listdir("./planilhas")
+    try:
+        arquivo_escolhido, index = pick(arquivos, "Arquivos na pasta de planilhas:")
+    except:
+        print("Arquivos na pasta de planilhas: ", arquivos)
+        arquivo_escolhido = input("Digite o nome completo do arquivo na pasta de planilhas: ")
+
+    if arquivo_escolhido[-4:] != "xlsx":
+        raise TypeError("Este arquivo não está no formato Excel \"xlsx\"")
+    return pd.read_excel(f"./planilhas/{arquivo_escolhido}")
+
+
+def criar_sessao():
+    print("Se conectando ao The Movie Database...")
+    apiKey = os.getenv("API_KEY")
+    url = "https://api.themoviedb.org/3/authentication"
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer " + apiKey
+    }
+    sess.headers.update(headers)
+    authRes = sess.get(url, headers=headers)
+
+
+def buscar_filme(titulo_filme: str) -> dict:
+    searchUrl = f"https://api.themoviedb.org/3/search/movie?query={titulo_filme}&include_adult=true&language=pt-BR&page=1"
+    searchRes = sess.get(searchUrl)
+    return searchRes.json()
+
+
+def formatar_filme(jsonf: dict) -> dict:
+    filme_correspond = {"popularidade": 0}
+
+    for filme in jsonf.get("results"):
+        if "popularity" not in filme:
+            filme.update({"popularity": 1})
+
+        if filme.get("popularity") > filme_correspond.get("popularidade"):
+            filme_correspond = {
+                "id": filme.get("id"),
+                "filme": filme.get("title"),
+                "popularidade": filme.get("popularity")
+            }
+    return filme_correspond
+
+
+def encontrar_palavras_chave(filme: dict) -> dict:
+    categRes = sess.get(f"https://api.themoviedb.org/3/movie/{filme.get('id')}/keywords")
+    jsonCategs = categRes.json()
+
+    categorias = ""
+    if categRes.status_code == 200:
+        for categoria in jsonCategs.get("keywords"):
+            categorias += f' {categoria.get("name")}'
+
+    filme.update({"categorias": categorias})
+    return filme
+
+
+def gerar_wordcloud(categorias: str) -> WordCloud:
+    print("Gerando o Wordcloud...")
+    wc = WordCloud(background_color="white", repeat=False)
+    wc.generate(categorias)
+    plt.axis("off")
+    plt.imshow(wc, interpolation="bilinear")
+    plt.show()
+    return wc
+
+
+def balancear_pesos_notas() -> str:
+    filmes_e_notas = ler_arquivo()
+    filmes_encontrados = []
+    resultados = ""
+
+    print("Buscando e formatando filmes...")
+    for i in progressbar(filmes_e_notas.index.to_list()):
+        filme = formatar_filme(buscar_filme(filmes_e_notas["Filme"][i]))
+        filme = encontrar_palavras_chave(filme)
+        filmes_encontrados.append(filme.get("filme"))
+
+        for i in range(0, filmes_e_notas["Probabilidade de assistir"][i]):
+            resultados += f' {filme.get("categorias")}'
+
+    print("Filmes encontrados:")
+    print(filmes_encontrados)
+    print("/!\\ Se algum filme foi indexado incorretamente, favor checar seu nome no"
+          " The Movie Database como referência para atualizá-lo na planilha sendo lida.\n")
+    return resultados
+
+
+def salvar_wordcloud(wc: WordCloud):
+    print("Gravando Wordcloud na pasta \"wordclouds\"...")
+    try:
+        svg_wc = wc.to_svg(embed_font=True)
+        destino = os.getenv("DESTINO_GRAVACAO", "./wordclouds")
+        data_hora = dt.now().strftime("%d-%m-%Y %H:%M")
+        nome_arquivo = f'{destino}/wordcloud {data_hora}.svg'
+
+        f = open(nome_arquivo, "w+")
+        f.write(svg_wc)
+        f.close()
+        print("Sucesso!")
+    except Exception as e:
+        raise e
+
+criar_sessao()
+results = balancear_pesos_notas()
+wc = gerar_wordcloud(results)
+salvar_wordcloud(wc)
